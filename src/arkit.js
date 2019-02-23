c1ea03cd25c96326a7cfa692a16ff5fc55fa71a3
Marat Dyatko <marat.dyatko@naturalcycles.com>
2019-02-23 22:43:29 +0100

    update snapshots

---
+++ src/arkit.js
@@ -0,0 +1,44 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs = require("fs");
+const path = require("path");
+const logger_1 = require("./logger");
+const config_1 = require("./config");
+const parser_1 = require("./parser");
+const generator_1 = require("./generator");
+exports.arkit = (directory) => {
+    const config = new config_1.Config(directory);
+    logger_1.debug(`Config`);
+    logger_1.trace(config);
+    const parser = new parser_1.Parser(config);
+    const files = parser.parse();
+    logger_1.trace('Parsed files');
+    logger_1.trace(files);
+    const generator = new generator_1.Generator(config, files);
+    return config.outputs.map(output => {
+        const puml = generator.generatePlantUML(output);
+        if (output.path) {
+            for (const outputPath of config.array(output.path)) {
+                const fullExportPath = path.join(config.directory, outputPath);
+                const ext = path.extname(fullExportPath);
+                if (fs.existsSync(fullExportPath)) {
+                    fs.unlinkSync(fullExportPath);
+                }
+                if (ext === '.puml') {
+                    fs.writeFileSync(fullExportPath, puml);
+                }
+                if (ext === '.svg') {
+                    generator.convertToSVG(puml).then(svg => {
+                        fs.writeFileSync(fullExportPath, svg);
+                    }).catch(err => {
+                        throw err;
+                    });
+                }
+            }
+        }
+        else {
+            console.log(puml);
+        }
+        return puml;
+    });
+};
