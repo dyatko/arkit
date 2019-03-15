456711a408e6a239ad37d1aa4832717fef440291
Marat Dyatko <i@marat.by>
2019-03-15 14:59:39 +0100

    Add #31 test case

--- test/sample/controllers/singleton.ts
+++ test/sample/controllers/singleton.ts
@@ -1,9 +1,24 @@
-import { List, Model, CONSTANT, a, b1, b2, b3, c } from '../models/model'
+import { List, Model, CONSTANT, a, b1, b2, b3, c } from "../models/model";
 
 class Singleton {
   model: Model = {
     property: List.KEY
+  };
+
+  state = {
+    user: undefined
+  }
+
+  onStateChanged(evt) {
+    const isEmpty = value => value === "";
+    const isReset = isEmpty(evt.target.value);
+    const user = { ...this.state.user, [evt.target.id]: evt.target.value };
+    const isFormComplete = !isReset && !Object.values(user).some(isEmpty);
+    this.setState({ user, isFormComplete });
+  }
+
+  setState(state) {
   }
 }
 
-export const singleton = new Singleton()
+export const singleton = new Singleton();
